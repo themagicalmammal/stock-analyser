065674c4d054bfeb8cb095c7f3067aefddfe635c
themagicalmammal <themagicalmammal@users.noreply.github.com>
2022-04-10 15:57:30 +0000

    [autofix] Format Python

--- stock_analysis/__init__.py
+++ stock_analysis/__init__.py
@@ -1,6 +1,6 @@
 """Classes for making technical stock analysis easier."""
 
 from stock_analysis.stock_analyzer import StockAnalyzer
-from stock_analysis.stock_visualizer import AssetGroupVisualizer
 from stock_analysis.stock_modeler import StockModeler
-from stock_analysis.utils import group_stocks, describe_group, make_portfolio
+from stock_analysis.stock_visualizer import AssetGroupVisualizer
+from stock_analysis.utils import describe_group, group_stocks, make_portfolio
