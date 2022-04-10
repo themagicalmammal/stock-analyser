ba333b32537302d67e99980b83a9332f909417ef
themagicalmammal <dipangithub@gmail.com>
2022-04-10 17:37:14 +0100

    Pypi 0.3.4

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+with codecs.open(os.path.join(here, "Pypi.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.3"
+VERSION = "0.3.4"
 DESCRIPTION = "Classes for technical analysis of stocks."
 
 # Setting up
