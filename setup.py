c0780470476c8cb18d5696b3f0e0bbe3f94c57ce
Dipan Nanda <dipangithub@gmail.com>
2023-01-01 16:09:42 +0000

    0.3.5

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "Pypi.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.3.4"
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+VERSION = "0.3.5"
 DESCRIPTION = "Classes for technical analysis of stocks."
 
 # Setting up
@@ -19,15 +22,7 @@ setup(
     author_email="d19cyber@gmail.com",
     description=DESCRIPTION,
     packages=find_packages(),
-    install_requires=[
-        "matplotlib>=3.0.2",
-        "numpy>=1.15.2",
-        "pandas>=0.23.4",
-        "pandas-datareader>=0.7.0",
-        "seaborn>=0.11.0",
-        "statsmodels>=0.11.1",
-        "mplfinance>=0.12.7a4",
-    ],
+    install_requires=required,
     keywords=[
         "python",
         "stock",
