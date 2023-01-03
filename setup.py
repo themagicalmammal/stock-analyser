ef5312c821cfed7d1c1a3b0c86fc587cc54d2d70
Dipan Nanda <43346471+themagicalmammal@users.noreply.github.com>
2023-01-03 16:02:06 +0000

    0.3.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
-VERSION = "0.3.6"
+VERSION = "0.3.7"
 DESCRIPTION = "Classes for technical analysis of stocks."
 
 # Setting up
@@ -23,8 +23,19 @@ setup(
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
+    url="https://github.com/themagicalmammal/stock-analyser",
     packages=find_packages(),
-    install_requires=required,
+    include_package_data=True,
+    license="MIT",
+    install_requires=required,    
+    project_urls={
+        # "Website": "https://github.com/themagicalmammal/stock-analyser",
+        # "Source code": "https://github.com/themagicalmammal/stock-analyser",
+        "Documentation":
+        "https://github.com/themagicalmammal/stock-analyser/blob/main/README.md",
+        "Bug tracker":
+        "https://github.com/themagicalmammal/stock-analyser/issues",
+    },
     keywords=[
         "python",
         "stock",
@@ -33,9 +44,13 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
+        "Topic :: Utilities",
     ],
 )
